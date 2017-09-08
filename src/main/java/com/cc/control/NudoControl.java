bed39757bf0893f73c8c058416055744dd04dcfd
Caleb.Chen <caleb.cheng@elitechtw01.com>
2017-09-08 16:22:33 +0800

    test

--- src/main/java/com/cc/control/NudoControl.java
+++ src/main/java/com/cc/control/NudoControl.java
@@ -95,6 +95,7 @@ public class NudoControl {
 	            g.dispose();
 	            
 	            response.setCharacterEncoding("UTF-8");
+	            response.setHeader("content-type","text/html;charset=UTF-8");
 		        OutputStream out = response.getOutputStream();
 		        ImageIO.write(image, "png", out);
 		        out.close();
